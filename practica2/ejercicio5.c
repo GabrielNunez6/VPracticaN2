1f011622cd39463dc72bc11c3596c41eb85ba48a
GabrielNunez6 <gabriel.nunez.saldana@uabc.edu.mx>
2025-09-11 13:11:28 -0700

    Ejercicio #5 Practica 2

---
+++ practica2/ejercicio5.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include "aleatorio.h"
+
+int main() {
+
+    int dado = lanzarDado();
+    printf("El dado cayo en: %d\n", dado);
+    switch (dado) {
+        case 1:
+        case 2:
+        case 3:
+            printf("*\n");
+            break;
+        case 4:
+            printf("L\n");
+            break;
+        case 5:
+            printf("C\n");
+            break;
+        case 6:
+            printf("R\n");
+            break;
+        default:
+    }
+
+    return 0;
+}
