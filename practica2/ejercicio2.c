860c9a2ce38104aaa2e8ed2b2414305799e41cd0
GabrielNunez6 <gabriel.nunez.saldana@uabc.edu.mx>
2025-09-11 12:51:45 -0700

    Ejercicio #2 Practica2

---
+++ practica2/ejercicio2.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include "aleatorio.h"
+
+int main() {
+
+    int d1 = lanzarDado();
+    int d2 = lanzarDado();
+    int d3 = lanzarDado();
+    int temporal;
+    printf("Dados obtenidos: %d, %d, %d\n", d1, d2, d3);
+    if (d1 < d2) {
+        temporal = d1;
+        d1 = d2;
+        d2 = temporal; }
+    if (d1 < d3) {
+        temporal = d1;
+        d1 = d3;
+        d3 = temporal;
+    }
+    if (d2 < d3) {
+        temporal = d2;
+        d2 = d3;
+        d3 = temporal;
+    }
+
+    printf("Orden descendente: %d, %d, %d\n", d1, d2, d3);
+
+    return 0;
+}
