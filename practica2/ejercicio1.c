6ed0a62f451d118839d58a81693d5b04c0f33991
GabrielNunez6 <gabriel.nunez.saldana@uabc.edu.mx>
2025-09-11 12:37:43 -0700

    Ejercicio #1 Practica2

---
+++ practica2/ejercicio1.c
@@ -0,0 +1,23 @@
+//
+// Created by usuario on 4/8/2025.
+//
+#include <stdio.h>
+#include "aleatorio.h"
+
+int main() {
+
+    int dado1=lanzarDado();
+    int dado2=lanzarDado();
+    int dado3=lanzarDado();
+
+    int mayor = dado1;
+    if (dado2>mayor)
+        mayor = dado2;
+    if (dado3>mayor)
+        mayor = dado3;
+
+    printf("Los 3 dados lanzados fueron: %d, %d, %d\n", dado1, dado2, dado3);
+    printf("El dado mayor es: %d\n", mayor);
+
+    return 0;
+}
