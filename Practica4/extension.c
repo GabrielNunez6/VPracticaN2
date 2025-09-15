8dbe99fdb17f1d60dc9410756efa3c7d88244a51
GabrielNunez6 <gabriel.nunez.saldana@uabc.edu.mx>
2025-09-15 14:33:31 -0700

    Funcion #2
     función que reciba la medida de los lados de un triángulo y regresa true si los lados forman un triángulo y false si no

--- Practica4/extension.c
+++ Practica4/extension.c
@@ -8,3 +8,12 @@
 float perimetroTriangulo(float lado1, float lado2, float lado3) {
     return lado1 + lado2 + lado3;
 }
+//función que reciba la medida de los lados de un triángulo y
+//regresa true si los lados forman un triángulo y false si no
+bool esTriangulo(float lado1, float lado2, float lado3) {
+    if (lado1 + lado2 > lado3 && lado1 + lado3 > lado2 && lado2 + lado3 > lado1) { //La suma de dos lados debe ser mayor al lado restante
+        return true;
+    } else {
+        return false;
+    }
+}
