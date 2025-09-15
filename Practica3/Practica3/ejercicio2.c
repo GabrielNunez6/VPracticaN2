c3b4e32594ca710cea8ba321c9af105ddcae7580
GabrielNunez6 <gabriel.nunez.saldana@uabc.edu.mx>
2025-09-15 14:02:16 -0700

    Le agregue los dos ciclos que es lo que pide
    al igual lo deje como en tabla para que se vea mas limpio

--- Practica3/Practica3/ejercicio2.c
+++ Practica3/Practica3/ejercicio2.c
@@ -1,46 +1,32 @@
 #include <stdio.h>
 
 int main() {
-    int palo, valor;
+    int palo = 1;
+    int valor;
 
-    for (palo = 1; palo <= 5; palo++) {
-        switch (palo) {
-            case 1:
-                printf("=== Corazones ===\n");
-                break;
-            case 2:
-                printf("=== Diamantes ===\n");
-                break;
-            case 3:
-                printf("=== Treboles ===\n");
-                break;
-            case 4:
-                printf("=== Picas ===\n");
-                break;
-            case 5:
-                printf("=== Estrellas ===\n");
-                break;
+    while (palo <= 5) {
+        if (palo == 1)
+            printf("\n=== Corazon ===\n");
+        else if (palo == 2)
+            printf("\n=== Diamante ===\n");
+        else if (palo == 3)
+            printf("\n=== Trebol ===\n");
+        else if (palo == 4)
+            printf("\n=== Picas ===\n");
+        else if (palo==5)
+            printf("\n=== Estrellas ===\n");
 
-        }
         for (valor = 3; valor <= 13; valor++) {
-
-            switch (valor) {
-                case 11:
-                    printf("J ");
-                    break;
-                case 12:
-                    printf("Q ");
-                    break;
-                case 13:
-                    printf("K ");
-                    break;
-                default:
-                    printf("%d ", valor);
-                    break;
-            }
+            if (valor == 11)
+                printf("J ");
+            else if (valor == 12)
+                printf("Q ");
+            else if (valor == 13)
+                printf("K ");
+            else
+                printf("%d ", valor);
         }
         printf("\n");
+        palo++;
     }
-
-    return 0;
 }
