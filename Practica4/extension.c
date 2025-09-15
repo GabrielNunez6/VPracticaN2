e21119bae7eb5a003c7891ab8fd726dd55293892
GabrielNunez6 <gabriel.nunez.saldana@uabc.edu.mx>
2025-09-15 14:25:17 -0700

    Funcion #1
    función que reciba la medida de los lados de un triángulo y regrese la medida de su perímetro.

---
+++ Practica4/extension.c
@@ -0,0 +1,10 @@
+//
+// Created by Gabriel Nunez Saldana on 9/15/2025.
+//
+
+//función que reciba la medida de los lados de un triángulo
+//y regrese la medida de su perímetro.
+
+float perimetroTriangulo(float lado1, float lado2, float lado3) {
+    return lado1 + lado2 + lado3;
+}
