8942a889055638bea11c299d08ec6824c9c7ae8e
GabrielNunez6 <gabriel.nunez.saldana@uabc.edu.mx>
2025-09-15 14:25:48 -0700

    Add files via upload

---
+++ Practica4/extension.h
@@ -0,0 +1,13 @@
+//
+// Created by Cecilia Gabriel Nunez on 8/15/25.
+//
+
+
+#ifndef ALEATORIO_H
+#define ALEATORIO_H
+#include <stdlib.h>
+#include <time.h>
+float perimetroTriangulo(float a, float b, float c);
+
+
+#endif //ALEATORIO_H
