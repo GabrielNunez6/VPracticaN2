df15b0c1eb3276a4d866bf4a2fd3fbd2cec90071
GabrielNunez6 <gabriel.nunez.saldana@uabc.edu.mx>
2025-09-22 11:55:50 -0700

    primer main que hice
    me falta hacer lo demas para mostrar el triangulo mas grande

--- Practica4/extension.h
+++ Practica4/extension.h
@@ -1,18 +1,14 @@
-//
-// Created by Cecilia Gabriel Nunez on 8/15/25.
-//
+#ifndef EXTENSION_H
+#define EXTENSION_H
 
-
-#ifndef ALEATORIO_H
-#define ALEATORIO_H
-#include <stdlib.h>
-#include <time.h>
 #include <stdbool.h>
-#include <math.h>
-float perimetroTriangulo(float lado1, float lado2, float lado3);
+
+// Funciones para triángulos
 bool esTriangulo(float lado1, float lado2, float lado3);
 bool esTrianguloEquilatero(float lado1, float lado2, float lado3);
-bool esTrianguloEscaleno(float lado1, float lado2, float lado3);
 bool esTrianguloIsosceles(float lado1, float lado2, float lado3);
+bool esTrianguloEscaleno(float lado1, float lado2, float lado3);
 float areaTriangulo(float lado1, float lado2, float lado3);
-#endif //ALEATORIO_H
+float perimetroTriangulo(float lado1, float lado2, float lado3);
+
+#endif
