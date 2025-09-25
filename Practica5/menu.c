9200d85e144b84142e513241549e5ddb6e6038b8
GabrielNunez6 <gabriel.nunez.saldana@uabc.edu.mx>
2025-09-25 14:18:37 -0700

    primer funcion del menu de opciones

---
+++ Practica5/menu.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+void mostrarMenu() {
+    int i = 0;
+    do {
+        printf("1. Opcion 1\n");
+        printf("2. Opcion 2\n");
+        printf("3. Opcion 3\n");
+        printf("4. Opcion 4\n");
+        i++;
+    } while (i < 1);
+}
---
+++ Practica5/menu.h
@@ -0,0 +1,6 @@
+
+
+#ifndef PRACTICA_MENU_H
+#define PRACTICA_MENU_H
+void mostrarMenu();
+#endif //PRACTICA_MENU_H
