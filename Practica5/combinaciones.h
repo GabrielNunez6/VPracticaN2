853f103d00cf6a2eaa2a4891001e57df6ddd507e
GabrielNunez6 <gabriel.nunez.saldana@uabc.edu.mx>
2025-09-29 13:29:41 -0700

    Add files via upload

---
+++ Practica5/combinaciones.h
@@ -0,0 +1,13 @@
+//
+// Created by usuario on 9/29/2025.
+//
+#ifndef COMBINACIONES_H
+#define COMBINACIONES_H
+
+
+int sumaSiIguales(int d1, int d2, int d3, int d4, int d5);
+int sumaTresIguales(int d1, int d2, int d3, int d4, int d5);
+int sumaDosIguales(int d1, int d2, int d3, int d4, int d5);
+int sumaSecuencia(int d1, int d2, int d3, int d4, int d5);
+
+#endif
