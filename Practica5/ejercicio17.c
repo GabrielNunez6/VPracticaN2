cebb3df2afadfce79bd1abb869b78fd3ccc0be91
GabrielNunez6 <gabriel.nunez.saldana@uabc.edu.mx>
2025-10-06 13:03:09 -0700

    Add files via upload

--- Practica5/ejercicio17.c
+++ Practica5/ejercicio17.c
@@ -4,14 +4,14 @@
 #include "menu.h"
 
 int main() {
-    int d1=0, d2=0, d3=0, d4=0, d5=0;
+    int d1 = 0, d2 = 0, d3 = 0, d4 = 0, d5 = 0;
     int opcion;
 
     do {
         MenuCombinaciones();
-        opcion = OpcionNumero(1,7);
+        opcion = OpcionNumero(1, 7);
 
-        switch(opcion) {
+        switch (opcion) {
             case 1: // Lanzar los 5 dados
                 d1 = 1;
                 d2 = 1;
@@ -27,22 +27,14 @@ int main() {
 
             case 3: // Balut
                 if (sumaSiIguales(d1, d2, d3, d4, d5)) {
-                    printf("\nBalut! La suma fue %d, Puntos:30\n", sumaSiIguales(d1,d2, d3, d4, d5));
+                    printf("\nBalut! La suma fue %d, Puntos: 30\n", sumaSiIguales(d1, d2, d3, d4, d5));
                 } else {
                     printf("No hay Balut.\n");
                 }
                 break;
 
-            case 4: // Secuencia
-                if (sumaSiEsSecuencia(d1, d2, d3, d4, d5)) {
-                    printf("Los dados forman una secuencia. La suma es %d\n",
-                           sumaSiEsSecuencia(d1,d2,d3,d4,d5));
-                } else {
-                    printf("No forman secuencia.\n");
-                }
-                break;
-            case 5: { // Full House
-                int full = sumaSiFullHouse(d1,d2,d3,d4,d5);
+            case 4: { // Full House
+                int full = sumaSiHayFullHouse(d1, d2, d3, d4, d5);
                 if (full) {
                     printf("Full House! Suma: %d. Puntos: 28\n", full);
                 } else {
@@ -51,11 +43,21 @@ int main() {
                 break;
             }
 
-            case 7: // Termina el programa
-                printf("Terminar\n");
+            case 5: // Secuencia baja (1 al 5)
+                if (esSecuenciaBaja(d1, d2, d3, d4, d5)) {
+                    printf("Hay una secuencia baja (1 al 5)\n");
+                } else {
+                    printf("No hay secuencia baja.\n");
+                }
                 break;
+
+            case 7: // Terminar el programa
+                printf("Programa terminado.\n");
+                break;
+
         }
-    } while(opcion != 7);
+
+    } while (opcion != 7);
+
     return 0;
 }
-
