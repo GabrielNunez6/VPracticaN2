fc4ab6fcf1227b5f38fada7d7ebfeb2bb4fea188
GabrielNunez6 <gabriel.nunez.saldana@uabc.edu.mx>
2025-10-06 15:28:02 -0700

    Add files via upload

---
+++ Practica5/ejercicio5.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include "aleatorio.h"
+#include "menu.h"
+
+
+int main() {
+    char opcion;
+    int tamano;
+    int alto, largo;
+
+
+    do {
+        mostrarMenu();
+        opcion = OpcionLetra('a', 'g');
+
+
+        switch(opcion) {
+            case 'a': // Rectángulo pide alto y largo
+                printf("Introduce la altura: ");
+                scanf("%d", &alto);
+                printf("Introduce el largo: ");
+                scanf("%d", &largo);
+                figura1(alto, largo);
+                break;
+            case 'b':
+                printf("Introduce el tamaño del triángulo: ");
+                scanf("%d", &tamano);
+                figura2(tamano);
+                break;
+            case 'c':
+                printf("Introduce el tamaño del triángulo: ");
+                scanf("%d", &tamano);
+                figura3(tamano);
+                break;
+            case 'd':
+                printf("Introduce el tamano del triangulo: ");
+                scanf("%d", &tamano);
+                figura4(tamano);
+                break;
+            case 'e':
+                printf("Introduce el tamano del triangulo: ");
+                scanf("%d", &tamano);
+                figura5(tamano);
+                break;
+            case 'f':
+                printf("Introduce el tamano del triangulo: ");
+                scanf("%d", &tamano);
+                figura6(tamano);
+                break;
+            case 'g':
+                printf("Salir\n");
+                break;
+        }
+
+    } while(opcion != 'g');
+    return 0;
+}
--- Practica5/menu.c
+++ Practica5/menu.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include "menu.h"
 
+// Función que muestra el menú de figuras en pantalla
 void mostrarMenu() {
     printf("\n=== MENU DE FIGURAS ===\n");
     printf("a. Rectangulo de asteriscos\n");
@@ -12,27 +13,33 @@ void mostrarMenu() {
     printf("g. Salir\n");
 }
 
+// Función que recibe dos letras (inicio y fin),
+// que mediante un ciclo do-while pide al usuario una opción válida y
+// regresa la letra elegida
 char OpcionLetra(char inicio, char fin) {
     char opcion;
     do {
         printf("\nSelecciona una opcion (%c - %c):\n ", inicio, fin);
-        scanf("%c", &opcion);
+        scanf(" %c", &opcion);
         getchar();
     } while (opcion < inicio || opcion > fin);
     return opcion;
 }
 
+// Función que recibe dos enteros (inicio y fin),
+// que mediante un ciclo do-while pide al usuario un número válido y
+// regresa el número elegido
 int OpcionNumero(int inicio, int fin) {
-    char opcion;
+    int opcion;
     do {
         printf("Selecciona una opcion (%d - %d):\n ", inicio, fin);
-        scanf(" %d", &opcion);
+        scanf("%d", &opcion);
     } while (opcion < inicio || opcion > fin);
     return opcion;
 }
 
-//Funcion que recibe como parametros el alto y el largo de la figura para despues
-//mediante un ciclo for imprimirlos en asteriscos formando un rectangulo
+// Función que recibe alto y largo, que mediante un ciclo externo recorre filas y
+// un ciclo interno recorre columnas para imprimir asteriscos
 void figura1(int alto, int largo) {
     for(int i = 0; i < alto; i++) {
         for(int j = 0; j < largo; j++) {
@@ -41,8 +48,9 @@ void figura1(int alto, int largo) {
         printf("\n");
     }
 }
-//funcion que recibe el tamano de un rectangulo que mediante un ciclo for
-//lo muestra alineado a la izquierda
+
+// Función que recibe tamaño, que mediante un ciclo externo recorre filas y
+// un ciclo interno imprime asteriscos de cada fila para formar un triángulo izquierdo
 void figura2(int tamano) {
     for(int i = 0; i <= tamano; i++) {
         for(int j = 0; j < i; j++) {
@@ -52,6 +60,9 @@ void figura2(int tamano) {
     }
 }
 
+// Función que recibe tamaño, que mediante un ciclo externo recorre filas,
+// un primer ciclo interno imprime espacios y
+// un segundo ciclo interno imprime asteriscos para formar un triángulo derecho
 void figura3(int tamano) {
     for (int i = 0; i <= tamano; i++) {
         for (int j = tamano; j > i; j--) {
@@ -65,10 +76,10 @@ void figura3(int tamano) {
     printf("\n");
 }
 
-// Función que recibe el tamaño de un triángulo y lo imprime con asteriscos.
-// Usa 3 ciclos for: el primero controla las filas, el segundo imprime espacios
-// y el tercero los asteriscos con una multiplicacion deL TAMANO por 2 - 1
-//para que se forme la piramide ya que va en numeros impares.
+// Función que recibe tamaño, que mediante un ciclo externo recorre filas,
+// un primer ciclo interno imprime espacios y
+// un segundo ciclo interno imprime asteriscos
+// (cantidad impar) para formar una pirámide centrada
 void figura4(int tamano) {
     for(int i = 0; i <= tamano; i++) {
         for(int j = 0; j < tamano - i; j++) {
@@ -81,3 +92,43 @@ void figura4(int tamano) {
     }
 }
 
+// Función que recibe tamaño, que mediante un ciclo externo recorre filas,
+// un primer ciclo interno imprime espacios,
+// un segundo ciclo interno imprime números de izquierda a derecha y
+// un tercer ciclo interno imprime números de derecha a izquierda para formar
+// una pirámide de números iguales
+void figura5(int tamano) {
+    for (int i = 0; i <= tamano; i++) {
+        for (int j = 30; j > i; j--) {
+            printf(" ");
+        }
+        for (int j = 1; j <= i; j++) {
+            printf("%d", i);
+        }
+        for (int j = 1; j < i; j++) {
+            printf("%d", i);
+        }
+        printf("\n");
+    }
+    printf("\n");
+}
+
+// Función que recibe tamaño, que mediante un ciclo externo recorre filas,
+// un primer ciclo interno imprime espacios,
+// un segundo ciclo interno imprime números decrecientes y
+// un tercer ciclo interno imprime números ascendentes para f
+// ormar una pirámide numérica decreciente-ascendente
+void figura6(int tamano) {
+    for (int i = 0; i <= tamano; i++) {
+        for (int j = 30; j > i; j--) {
+            printf(" ");
+        }
+        for (int j = i; j >= 1; j--) {
+            printf("%d", j);
+        }
+        for (int j = 2; j <= i; j++) {
+            printf("%d", j);
+        }
+        printf("\n");
+    }
+}
