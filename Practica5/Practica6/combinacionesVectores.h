adbd4bace8e27500265493d6fcb035c8a16b3028
GabrielNunez6 <gabriel.nunez.saldana@uabc.edu.mx>
2025-10-12 13:13:39 -0700

    Add files via upload

---
+++ Practica5/Practica6/combinacionesVectores.h
@@ -0,0 +1,8 @@
+//
+// Created by usuario on 10/10/2025.
+//
+
+#ifndef P5_COMBINACIONESVECTORES_H
+#define P5_COMBINACIONESVECTORES_H
+int sumaSiIgualesVectores(int numeros[], int tamaño);
+#endif //P5_COMBINACIONESVECTORES_H
