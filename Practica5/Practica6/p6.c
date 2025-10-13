b942f3603c056a07a338c0b66a3c497cf87a12a1
GabrielNunez6 <gabriel.nunez.saldana@uabc.edu.mx>
2025-10-13 10:24:57 -0700

    Add files via upload

--- Practica5/Practica6/p6.c
+++ Practica5/Practica6/p6.c
@@ -4,9 +4,8 @@
 #include "menu.h"
 #include "combinaciones.h"
 
-
 int main() {
-    int d1=0, d2=0, d3=0, d4=0, d5=0;
+    int dados[5] = {0};
     int opcion;
 
     do {
@@ -14,34 +13,69 @@ int main() {
         opcion = OpcionNumero(1,7);
 
         switch(opcion) {
-            case 1: // Lanzar los 5 dados
-                d1 = 1;
-                d2 = 1;
-                d3 = 1;
-                d4 = 2;
-                d5 = 2;
+            case 1: // Lanzar dados
+                dados[0] = 2;
+                dados[1] = 3;
+                dados[2] = 4;
+                dados[3] = 5;
+                dados[4] = 6;
                 printf("Dados lanzados.\n");
                 break;
 
-            case 2: // Mostrar los 5 dados
-                printf("Los dados lanzados fueron: %d %d %d %d %d\n", d1, d2, d3, d4, d5);
+            case 2: // Mostrar dados
+                printf("Los dados lanzados fueron: ");
+                for(int i = 0; i < 5; i++)
+                    printf("%d ", dados[i]);
+                printf("\n");
                 break;
 
             case 3: // Balut
-                int dados[] = {d1, d2, d3, d4, d5};
+            {
                 int puntos = sumaSiIgualesVectores(dados, 5);
-
-                if (puntos > 0) {
-                    printf("\n¡Balut! Puntos: 30\n");
-                } else {
+                if(puntos > 0)
+                    printf("¡Balut! Puntos: 30\n");
+                else
                     printf("No hay Balut.\n");
-                }
                 break;
+            }
 
+            case 4: // Full House
+            {
+                int puntos = fullHouse(dados, 5);
+                if(puntos > 0)
+                    printf("¡Full House! Puntos: 28\n");
+                else
+                    printf("No es Full House.\n");
+                break;
+            }
 
+            case 5: // Secuencia baja (1-5)
+            {
+                int puntos = esSecuenciaBajaVectores(dados, 5);
+                if(puntos > 0)
+                    printf("Secuencia baja (1-5)! Puntos: 15\n");
+                else
+                    printf("No es secuencia baja.\n");
+                break;
+            }
+
+            case 6: // Secuencia alta (2-6)
+            {
+                int puntos = esSecuenciaAltaVectores(dados, 5);
+                if(puntos > 0)
+                    printf("Secuencia alta (2-6)! Puntos: 20\n");
+                else
+                    printf("No es secuencia alta.\n");
+                break;
+            }
+
+            case 7: // Terminar
+                printf("Programa terminado.\n");
+                break;
         }
 
-
     } while(opcion != 7);
+
     return 0;
 }
+
