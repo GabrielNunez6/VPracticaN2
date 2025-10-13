8e713661c02f56a32810cf12ba7121a6d293f62b
GabrielNunez6 <gabriel.nunez.saldana@uabc.edu.mx>
2025-10-13 10:25:18 -0700

    Add files via upload

--- Practica5/Practica6/combinacionesVectores.c
+++ Practica5/Practica6/combinacionesVectores.c
@@ -103,3 +103,114 @@ int fullHouse(int numeros[], int tamano) {
     else
         return 0;
 }
+
+//6)
+int sumaDeUnosVectores(int numeros[], int tamano) {
+    int suma = 0;
+
+    for (int i = 0; i < tamano; i++)
+        if (numeros[i] == 1)
+            suma += 1;
+
+    return suma;
+}
+
+//7)
+int sumaDeDosVectores(int numeros[], int tamano) {
+    int suma = 0;
+
+    for (int i = 0; i < tamano; i++)
+        if (numeros[i] == 2)
+            suma += 2;
+
+    return suma;
+}
+
+//8)
+int sumaDeTresVectores(int numeros[], int tamano) {
+    int suma = 0;
+
+    for (int i = 0; i < tamano; i++)
+        if (numeros[i] == 3)
+            suma += 3;
+
+    return suma;
+}
+
+//9)
+int sumaDeCuatrosVectores(int numeros[], int tamano) {
+    int suma = 0;
+
+    for (int i = 0; i < tamano; i++)
+        if (numeros[i] == 4)
+            suma += 4;
+
+    return suma;
+}
+
+//10)
+int sumaDeCincoVectores(int numeros[], int tamano) {
+    int suma = 0;
+
+    for (int i = 0; i < tamano; i++)
+        if (numeros[i] == 5)
+            suma += 5;
+
+    return suma;
+}
+
+//11)
+int sumaDeSeisVectores(int numeros[], int tamano) {
+    int suma = 0;
+
+    for (int i = 0; i < tamano; i++)
+        if (numeros[i] == 6)
+            suma += 6;
+
+    return suma;
+}
+
+//12)
+int esSecuenciaBajaVectores(int numeros[], int tamaño) {
+    for (int i = 0; i < tamaño - 1; i++) {
+        for (int j = 0; j < tamaño - i - 1; j++) {
+            if (numeros[j] > numeros[j + 1]) {
+                int temp = numeros[j];
+                numeros[j] = numeros[j + 1];
+                numeros[j + 1] = temp;
+            }
+        }
+    }
+    if (numeros[0] != 1 || numeros[tamaño - 1] != 5) {
+        return 0;
+    }
+    for (int i = 1; i < tamaño; i++) {
+        if (numeros[i] != numeros[i - 1] + 1) {
+            return 0;
+        }
+    }
+    return 1;
+}
+
+//13)
+int esSecuenciaAltaVectores(int numeros[], int tamaño) {
+    for (int i = 0; i < tamaño - 1; i++) {
+        for (int j = 0; j < tamaño - i - 1; j++) {
+            if (numeros[j] > numeros[j + 1]) {
+                int temp = numeros[j];
+                numeros[j] = numeros[j + 1];
+                numeros[j + 1] = temp;
+            }
+        }
+    }
+    if (numeros[0] != 2 || numeros[tamaño - 1] != 6) {
+        return 0;
+    }
+    for (int i = 1; i < tamaño; i++) {
+        if (numeros[i] != numeros[i - 1] + 1) {
+            return 0;
+        }
+    }
+
+    return 1;
+}
