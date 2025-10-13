507acd94b994e34bd8b4b577878517c1512c8627
GabrielNunez6 <gabriel.nunez.saldana@uabc.edu.mx>
2025-10-13 09:12:49 -0700

    Add files via upload

--- Practica5/Practica6/combinacionesVectores.c
+++ Practica5/Practica6/combinacionesVectores.c
@@ -54,3 +54,52 @@ int sumaSiDosIgualesVectores(int numeros[], int tamaño) {
 }
 
 //4)
+int sumaSiSecuenciaVectores(int numeros[], int tamaño) {
+    for (int i = 0; i < tamaño - 1; i++) {
+        for (int j = 0; j < tamaño - i - 1; j++) {
+            if (numeros[j] > numeros[j + 1]) {
+                int temp = numeros[j];
+                numeros[j] = numeros[j + 1];
+                numeros[j + 1] = temp;
+            }
+        }
+    }
+    for (int i = 1; i < tamaño; i++) {
+        if (numeros[i] != numeros[i - 1] + 1) {
+            return 0;
+        }
+    }
+    int suma = 0;
+    for (int i = 0; i < tamaño; i++) {
+        suma += numeros[i];
+    }
+    return suma;
+}
+
+//5)
+int fullHouse(int numeros[], int tamano) {
+    int i, j, iguales;
+    int tieneTercia = 0, tienePar = 0;
+    int suma = 0;
+    for (i = 0; i < tamano; i++) {
+        suma += numeros[i];
+    }
+
+    for (i = 0; i < tamano; i++) {
+        iguales = 1;
+        for (j = 0; j < tamano; j++) {
+            if (i != j && numeros[i] == numeros[j]) {
+                iguales++;
+            }
+        }
+        if (iguales == 3)
+            tieneTercia = 1;
+        if (iguales == 2)
+            tienePar = 1;
+    }
+
+    if (tieneTercia && tienePar)
+        return suma;
+    else
+        return 0;
+}
