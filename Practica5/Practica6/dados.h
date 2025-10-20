821168a9eac05eda667cb065c5670bcabc97c289
GabrielNunez6 <gabriel.nunez.saldana@uabc.edu.mx>
2025-10-20 01:41:13 -0700

    Add files via upload

---
+++ Practica5/Practica6/dados.h
@@ -0,0 +1,8 @@
+#ifndef DADOS_H
+#define DADOS_H
+
+void tirarDados(int dados[], int cantidad);
+void mostrarDados(int dados[], int cantidad);
+void relanzarDados(int dados[], int cantidad);
+
+#endif
