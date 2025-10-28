d3d0e5c22ff03e093b7d230e168a4e251f8fceb7
GabrielNunez6 <gabriel.nunez.saldana@uabc.edu.mx>
2025-10-27 17:11:42 -0700

    Add files via upload

---
+++ Practica7/BarajaInglesa.h
@@ -0,0 +1,24 @@
+//
+// Created by Cecilia Curlango on 9/9/25.
+//
+
+#ifndef ESTRUCTURASCONCARTAS_BARAJAINGLESA_H
+#define ESTRUCTURASCONCARTAS_BARAJAINGLESA_H
+#include <stdio.h>
+#include <stdbool.h>
+#include "aleatorio.h"
+
+typedef struct{
+    int valor;
+    char palo;
+}Carta;
+
+void mostrarCarta(Carta carta);
+bool compararValor(Carta uno, Carta dos);
+Carta generarCartaAleatoria();
+int sumarComo1(Carta uno, Carta dos, Carta tres);
+int sumarComo11(Carta uno, Carta dos, Carta tres);
+void llenarYMezclarBaraja(Carta carta[], int tamano); //FUNCION AÑADIDA
+
+
+#endif //ESTRUCTURASCONCARTAS_BARAJAINGLESA_H
---
+++ Practica7/Poker3Card.h
@@ -0,0 +1,8 @@
+//
+// Created by usuario on 10/27/2025.
+//
+
+#ifndef P7_POKER3CARD_H
+#define P7_POKER3CARD_H
+
+#endif //P7_POKER3CARD_H
