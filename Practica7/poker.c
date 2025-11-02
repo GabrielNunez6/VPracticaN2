2dfc850a5712819df61c5f86f8eba384d948d380
GabrielNunez6 <gabriel.nunez.saldana@uabc.edu.mx>
2025-11-02 01:12:14 -0700

    Add files via upload

--- Practica7/poker.c
+++ Practica7/poker.c
@@ -1,18 +1,44 @@
 #include <stdio.h>
-#include "aleatorio.h"
 #include "BarajaInglesa.h"
-#define CARTAS 52
+#include "Poker3Card.h"
+
+#define MAX_JUGADORES 4
+#define TAMANO_MANO 3
 
 int main() {
     initAleatorio();
-    Carta mazo[CARTAS];
+    printf("=== POKER DE 3 CARTAS===\n");
 
-    llenarYMezclarBaraja(mazo, CARTAS);
+    int numJugadores;
+    do {
+        printf("Cuantos jugadores participaran? (1-4): ");
+        scanf("%d", &numJugadores);
+        if (numJugadores < 1 || numJugadores > MAX_JUGADORES) {
+            printf("Ingresa un valor entre 1 y 4.\n");
+        }
+    } while (numJugadores < 1 || numJugadores > MAX_JUGADORES);
 
-    printf("10 cartas del mazo:\n");
-    for (int i = 0; i < 10; i++) {
-        printf("Carta %d: %d de %c\n", i+1, mazo[i].valor, mazo[i].palo);
+    Jugador jugadores[MAX_JUGADORES];
+    for (int i = 0; i < numJugadores; i++) {
+        jugadores[i].id = i + 1;
+        jugadores[i].saldo = 300;
+        jugadores[i].rondasJugadas = 0;
     }
+    Jugador dealer;
+    Carta mazo[52];
+    char continuar = 's';
 
-    return 0;
+
+    while (continuar == 's') {
+        printf("\n--- SIGUIENTE RONDA ---\n");
+        llenarYMezclarBaraja(mazo, 52);
+
+        int contador = 0;
+
+        for (int i = 0; i < numJugadores; i++) {
+            for (int j = 0; j < TAMANO_MANO; j++) {
+                jugadores[i].cartas[j] = mazo[contador++];
+            }
+        }
+    }
 }
