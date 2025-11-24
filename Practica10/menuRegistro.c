ba3dc059e02dbdcfffb9f6060e2a4b6f5d23a98c
GabrielNunez6 <gabriel.nunez.saldana@uabc.edu.mx>
2025-11-24 14:03:52 -0800

    Add files via upload

---
+++ Practica10/menuRegistro.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+//
+// Created by usuario on 11/24/2025.
+//
+void mostrarMenu() {
+    printf("\n===== MENU DE REGISTROS =====\n");
+    printf("1. Agregar registro\n");
+    printf("2. Eliminar registro\n");
+    printf("3. Corregir registro\n");
+    printf("4. Mostrar todos los registros\n");
+    printf("5. Guardar registros en archivo binario\n");
+    printf("6. Cargar registros desde archivo binario\n");
+    printf("0. Salir\n");
+    printf("Seleccione una opcion: ");
+}
---
+++ Practica10/menuRegistro.h
@@ -0,0 +1,9 @@
+//
+// Created by usuario on 11/24/2025.
+//
+
+#ifndef PRACTICA10_MENUREGISTRO_H
+#define PRACTICA10_MENUREGISTRO_H
+void mostrarMenu();
+
+#endif //PRACTICA10_MENUREGISTRO_H
